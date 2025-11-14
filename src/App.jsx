fb32bc0b48545779019f75bc70bd3cf22b728c18
enrico-antonini1 <enry1997@me.com>
2025-11-14 11:12:50 +0100

    fix: correzione a lezione

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-const movie = [
+const movies = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -9,34 +9,87 @@ const movie = [
   { title: 'Pulp Fiction', genre: 'Thriller' },
 ]
 
+const genre = [
+  "Fantascienza",
+  "Thriller",
+  "Romantico",
+  "Azione"
+]
+
+// Creare array di generi dinamicamente
+// 1) inizio creando un nuovo array vuoto
+const genres = [];
+// 2) faccio un forEach e pusho il genere solo se non è già presente
+movies.forEach((curMovie) => {
+  if (!genres.includes(curMovie.genre)) {
+    genres.push(curMovie.genre);
+  }
+
+});
+console.log(genres);
+
 
 
 function App() {
 
-  // Variabile dove salvo scelta del selected
-const [selectedGenre, setSelectedGenre] = useState("");
+  const [moviesArray, setMoviesArray] = useState(movies)
 
-function addMovie(event) {
-  event.preventDefault();
-  
-}
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  const [filteredMovies, setFilteredMovies] = useState(moviesArray);
+
 
   // useEffect( () => {
+  //   console.log("mounting")
+  //   return () => {
+  //     console.log("unmounting")
+  //   }
+  //  }, []);
 
-  // }, []);
+  useEffect(() => {
+    // Mi serve condizione if per dire che con "tutti" si devono vedere tutti, altrimenti di filtrare
+    if (selectedGenre === "") {
+      setFilteredMovies(moviesArray);
+    } else {
+      setFilteredMovies((prev) => moviesArray.filter((curMovie) => curMovie.genre === selectedGenre)
+      );
+    }
+  }, [selectedGenre]);
 
   return (
-    <> 
-    <div className="container">
-      <select 
-      className="form-select" 
-      aria-label="Default select example">
-        <option defaultValue>Seleziona genere film</option>
-        {movie.map((curMovie) => (
-        <option value="1">{curMovie.genre}</option>
-       ))}
-      </select>
-    </div>
+    <>
+      {/* Creazione select per scegliere genere film */}
+      <div className="container mt-4">
+        <select
+          className="form-select"
+          value={selectedGenre}
+          onChange={(event) => setSelectedGenre(event.target.value)}
+        >
+
+          <option defaultValue>Seleziona genere film</option>
+          <option value="">Tutti</option>
+
+          {genre.map((curGenre, index) => (
+            <option value={curGenre} key={index}>
+              {curGenre}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Creazione card con stampa dinamica dei film da array movies */}
+      <div className="row row-cols-3 mt-4 g-3">
+        {filteredMovies.map((curMovie, index) => (
+          <div className="col" key={index}>
+            <div className="card">
+              <div className="card-body">
+                <h3>{curMovie.title}</h3>
+                <p>{curMovie.genre}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
